6879d9588dc77ea66dc695854c580af7dc97d80a
kargm <kargm@in.tum.de>
2011-01-04 13:12:39 +0100

    Deleted build directory from repo

--- build/CMakeFiles/test_package_result.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/test_package_result"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/test_package_result.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/CMakeFiles/test_time_result.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/test_time_result"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/test_time_result.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
